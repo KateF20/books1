fe44557b9badb7fa65aeceba8f5d64bdce2bca51
Ekaterina Fedoseeva <kaitherinayrd@gmail.com>
2023-07-05 16:16:44 +0200

    using BookModel instead of Book to instantiate BOOKS data

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ from typing import Annotated
 from fastapi import FastAPI, Path, Query, HTTPException
 from starlette import status
 
-from models import Book, BookModel
+from models import BookModel
+
 
 app = FastAPI()
 
-
 BOOKS = [
-    Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5, 2020),
-    Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5, 2002),
-    Book(3, 'Master Endpoints', 'codingwithroby', 'An awesome book!', 5, 2019),
-    Book(4, 'HP1', 'Author 1', 'Book Description', 2, 2018),
-    Book(5, 'HP2', 'Author 2', 'Book Description', 3, 2017),
-    Book(6, 'HP3', 'Author 3', 'Book Description', 1, 2016)
+    BookModel(id=1, title='Computer Science Pro', author='codingwithroby', description='A very nice book!', rating=5, published_date=2020),
+    BookModel(id=2, title='Be Fast with FastAPI', author='codingwithroby', description='A great book!', rating=5, published_date=2002),
+    BookModel(id=3, title='Master Endpoints', author='codingwithroby', description='An awesome book!', rating=5, published_date=2019),
+    BookModel(id=4, title='HP1', author='Author 1', description='Book Description', rating=2, published_date=2018),
+    BookModel(id=5, title='HP2', author='Author 2', description='Book Description', rating=3, published_date=2017),
+    BookModel(id=6, title='HP3', author='Author 3', description='Book Description', rating=1, published_date=2016)
 ]
 
 
-@app.get('/books/{book_id}', status_code=status.HTTP_200_OK)
+@app.get('/books/{book_id}')
 async def get_book_by_id(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
@@ -45,12 +45,12 @@ async def get_book_by_query(
 
 
 @app.post('/create-book', status_code=status.HTTP_201_CREATED)
-async def create_book(book_request: BookModel):
-    new_book = Book(**book_request.dict())
+async def create_book(book_model: BookModel):
+    new_book = BookModel(**book_model.dict())
     BOOKS.append(get_book_id(new_book))
 
 
-def get_book_id(book: Book):
+def get_book_id(book: BookModel):
     book.id = BOOKS[-1].id + 1 if BOOKS else 1
     return book
 
@@ -58,8 +58,8 @@ def get_book_id(book: Book):
 @app.put('/books/update_book', status_code=status.HTTP_204_NO_CONTENT)
 async def update_book(book: BookModel):
     book_changed = False
-    for i in range(len(BOOKS)):
-        if BOOKS[i].id == book.id:
+    for i, current_book in enumerate(BOOKS):
+        if current_book.id == book.id:
             BOOKS[i] = book
             book_changed = True
     if not book_changed:
@@ -69,9 +69,9 @@ async def update_book(book: BookModel):
 @app.delete('/books/{book_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id: int = Path(gt=0)):
     book_changed = False
-    for i in range(len(BOOKS)):
-        if BOOKS[i].id == book_id:
-            BOOKS.pop(i)
+    for i, current_book in enumerate(BOOKS):
+        if current_book.id == book_id:
+            del BOOKS[i]
             book_changed = True
             break
     if not book_changed:
